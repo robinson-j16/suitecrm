b2f9811e22027c05a95dc5709f0eb9db94abdbfc
Matt Lorimer <matthew.lorimer@suitecrm.com>
2025-12-18 10:34:29 +0000

    SuiteCRM 7.15 Release

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -3,5 +3,5 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-$suitecrm_version = '7.14.8';
-$suitecrm_timestamp = '2025-11-06 12:00:00';
+$suitecrm_version = '7.15.0';
+$suitecrm_timestamp = '2025-12-18 12:00:00';
